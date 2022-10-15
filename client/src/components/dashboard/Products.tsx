c23e8a942c6cd3f3d1fccab99c751278f49fa732
Mark <jetlbade0@gmail.com>
2022-10-15 03:03:23 +0300

    initial-product-cards

--- client/src/components/dashboard/Products.tsx
+++ client/src/components/dashboard/Products.tsx
@@ -7,9 +7,20 @@ interface IShowTables {
 
 export const Products: FC<IShowTables> = ({ showTables, remove }) => {
   return (
-    <div className="table__arrangement">
-      zopa
-      <button onClick={remove}>Aboba</button>
+    <div className="product__list">
+      <div className="product__card">
+        <div className="product__picture">Picture</div>
+        <div className="product__bottom">
+          <div className="prod__b">
+            <div className="product__remainder">Остаток</div>
+            <div className="product__amount">5шт</div>
+            <div className="product__price">Цена: 100р</div>
+          </div>
+          <div className="prod__btn">
+            <button className="product__btn">Add to Cart</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
